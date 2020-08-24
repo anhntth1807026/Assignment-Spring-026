e51753dd61c825e83f06247e3358e2b4628fd97f
anhntth1807026 <anhntth1807026@fpt.edu.vn>
2020-08-24 22:14:59 +0700

    Assignment-spring-tuananh-026

---
+++ src/main/java/com/demo/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.demo.service;
+
+import com.demo.entity.Employee;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Page<Employee> getAllEmployees(int page, int limit);
+
+    void createEmp(Employee employee);
+
+    void updateEmp(Employee employee);
+
+    void deleteEmp(Integer id);
+
+    Employee getDetailEmp(Integer id);
+
+    List<Employee> searchByName(String q);
+}
